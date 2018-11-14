cd26ae29371e021072f77284dac733a98cc520e3
codabae <shalom.mat40@gmail.com>
2018-11-14 23:10:49 +0100

    gallery and connect page

--- src/Connect.js
+++ src/Connect.js
@@ -5,21 +5,58 @@ import './App.css';
 class Connect extends Component {
   render() {
     return (
-<div>
+<div className="formDiv">
+<h3 className='formHeader'>Send me a message</h3>
 <form>
-    <div class="form-group">
+    <div className="form-group">
       <input type="text" required="required"/>
-      <label for="input" class="control-label">Textfield</label><i class="bar"></i>
+      <label for="input" class="control-label">Full Name</label><i className="bar"></i>
     </div>
-    <div class="form-group">
+    <div className="form-group">
       <textarea required="required"></textarea>
-      <label for="textarea" class="control-label">Textarea</label><i class="bar"></i>
+      <label for="textarea" className="control-label">Message</label><i className="bar"></i>
     </div>
+
+  <button className="button">
+  Connect
+  </button>
+    </form>
+
+ <div className="formIconDiv">
+ 			
+ <a href="https://web.facebook.com/Shalom.Mathew40" class="Link">
+
+            <img src={require('./Img/icons/facebook.png')} alt="logo" className="IconsImg" />
+            </a>                       
+             <a href="https://twitter.com/ijeoma_s_mathew" class="Link">
+                                                                                                                                                                   
+            <img src={require('./Img/icons/twitter.png')} alt="logo" className="IconsImg" />
+                        </a>                       
+
+             <a href="https://www.linkedin.com/in/shalom-ijeoma-mathew-82124b16b/
+" class="Link">
+            <img src={require('./Img/icons/linkedin.png')} alt="logo" className="IconsImg" />
+                        </a>                  
+           
+             <a href="https://www.youtube.com/channel/UC7zntyTNDRf2HMinWgjJSog" class="Link">
+
+						<img src={require('./Img/icons/youtube.png')} alt="logo" className="IconsImg" />
+                                    </a>                       
+
+             <a href="https://github.com/CodaBae" class="Link">
+
+            <img src={require('./Img/icons/github.png')} alt="logo" className="IconsImg" />
+            </a>          
+ 
+ </div>
+
  
  
    
    
-  </form>
+
+  <span className="formSpan">+2348093546694</span>
+
 
 </div>
       );
--- src/Gallery.js
+++ src/Gallery.js
@@ -52,16 +52,46 @@ class Gallerys extends Component {
 
 				<div className='footerDiv'>
 					<div className="IconsDiv">
-						<img src={require('./Img/icons/facebook.png')} alt="logo" className="IconsImg" />
-						<img src={require('./Img/icons/twitter.png')} alt="logo" className="IconsImg" />
-						<img src={require('./Img/icons/linkedin.png')} alt="logo" className="IconsImg" />
-													<img src={require('./Img/icons/google-plus.png')} alt="logo" className="IconsImgG" />
+				
+ <a href="https://web.facebook.com/Shalom.Mathew40" class="Link">
 
-						<img src={require('./Img/icons/skype.png')} alt="logo" className="IconsImg" />
-						<img src={require('./Img/icons/instagram.png')} alt="logo" className="IconsImg" />
+            <img src={require('./Img/icons/facebook.png')} alt="logo" className="IconsImg" />
+            </a>                       
+             <a href="https://twitter.com/ijeoma_s_mathew" class="Link">
+                                                                                                                                                                   
+            <img src={require('./Img/icons/twitter.png')} alt="logo" className="IconsImg" />
+                        </a>                       
+
+             <a href="https://www.linkedin.com/in/shalom-ijeoma-mathew-82124b16b/
+" class="Link">
+            <img src={require('./Img/icons/linkedin.png')} alt="logo" className="IconsImg" />
+                        </a>                       
+
+             <a href="https://plus.google.com/u/0/108117543672886147456" class="Link">
+
+            <img src={require('./Img/icons/google-plus.png')} alt="logo" className="IconsImgG" />
+                        </a>                       
+
+             <a href="https://plus.google.com/u/0/108117543672886147456" class="Link">
+
+
+            <img src={require('./Img/icons/skype.png')} alt="logo" className="IconsImg" />
+                                    </a>                       
+
+             <a href="https://www.instagram.com/shalli.mat/" class="Link">
+
+            <img src={require('./Img/icons/instagram.png')} alt="logo" className="IconsImg" />
+                                    </a>                       
+
+             <a href="https://www.youtube.com/channel/UC7zntyTNDRf2HMinWgjJSog" class="Link">
+
 						<img src={require('./Img/icons/youtube.png')} alt="logo" className="IconsImg" />
-						<img src={require('./Img/icons/github.png')} alt="logo" className="IconsImg" />
+                                    </a>                       
 
+             <a href="https://github.com/CodaBae" class="Link">
+
+            <img src={require('./Img/icons/github.png')} alt="logo" className="IconsImg" />
+            </a>                                                                                                                                                                                          
 
 					</div>
 					<p className='footerText'>codabae</p>

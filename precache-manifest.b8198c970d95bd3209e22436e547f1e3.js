25d6c3c337a80e67ff7d332550d9f6b9e86139d8
codabae <shalom.mat40@gmail.com>
2018-11-21 16:10:23 +0100

    Updates

---
+++ precache-manifest.b8198c970d95bd3209e22436e547f1e3.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = [
+  {
+    "revision": "df339491c79698aa7ba933c5a448f5d6",
+    "url": "/codabae/static/media/g.df339491.JPG"
+  },
+  {
+    "revision": "0b7c2ebc44c74425a708",
+    "url": "/codabae/static/css/main.6cb7be59.chunk.css"
+  },
+  {
+    "revision": "0c718710ed8c49aade0d",
+    "url": "/codabae/static/js/1.0c718710.chunk.js"
+  },
+  {
+    "revision": "2cc3be72a5f1402f8027",
+    "url": "/codabae/static/js/runtime~main.2cc3be72.js"
+  },
+  {
+    "revision": "a1f31faea14c7e96931ff44cbc8fd140",
+    "url": "/codabae/static/media/Laptop.a1f31fae.jpg"
+  },
+  {
+    "revision": "e5e95142b5b3365added0517591caaa6",
+    "url": "/codabae/static/media/kir.e5e95142.png"
+  },
+  {
+    "revision": "70e9b9725630e8c299f05b9169afbbf6",
+    "url": "/codabae/static/media/recycling.70e9b972.jpg"
+  },
+  {
+    "revision": "46f548b79908f441895aa04b929dcbf2",
+    "url": "/codabae/static/media/lf.46f548b7.png"
+  },
+  {
+    "revision": "540aa89660963563cc131c72cd48eb9d",
+    "url": "/codabae/static/media/a.540aa896.jpg"
+  },
+  {
+    "revision": "fb19270007a4fc86b9efc6bd79ecc747",
+    "url": "/codabae/static/media/h.fb192700.jpg"
+  },
+  {
+    "revision": "31d97c3b3d65029674cf2fe607dd6522",
+    "url": "/codabae/static/media/toda.31d97c3b.jpeg"
+  },
+  {
+    "revision": "5515e6547e6a41795b0c6498b02b3dee",
+    "url": "/codabae/static/media/1.5515e654.jpg"
+  },
+  {
+    "revision": "99945749d0745b74b76a7e708f65de2c",
+    "url": "/codabae/static/media/2.99945749.jpg"
+  },
+  {
+    "revision": "7dc5a8b85b159bdc171818b44f0828cf",
+    "url": "/codabae/static/media/3.7dc5a8b8.jpg"
+  },
+  {
+    "revision": "1a0c7f38499e5a8622087e6b96e05929",
+    "url": "/codabae/static/media/a.1a0c7f38.JPG"
+  },
+  {
+    "revision": "8ac599abcd18ed264a5dfc66c5e319b4",
+    "url": "/codabae/static/media/b.8ac599ab.JPG"
+  },
+  {
+    "revision": "d95c3c67b2248fadab46c737113dd8f7",
+    "url": "/codabae/static/media/c.d95c3c67.JPG"
+  },
+  {
+    "revision": "667e075ce1a7bf4b41f3beef54395bae",
+    "url": "/codabae/static/media/d.667e075c.JPG"
+  },
+  {
+    "revision": "6423f7400b225de652520ad163cfccae",
+    "url": "/codabae/static/media/e.6423f740.JPG"
+  },
+  {
+    "revision": "acbe2fbc71ecf634b946e5fc1a530f8e",
+    "url": "/codabae/static/media/f.acbe2fbc.JPG"
+  },
+  {
+    "revision": "0b7c2ebc44c74425a708",
+    "url": "/codabae/static/js/main.0b7c2ebc.chunk.js"
+  },
+  {
+    "revision": "c952ef66ee6f7ac267d58f70ef00390a",
+    "url": "/codabae/static/media/h.c952ef66.JPG"
+  },
+  {
+    "revision": "a9bff4951c2350d6afcce17cc3bfd789",
+    "url": "/codabae/static/media/i.a9bff495.JPG"
+  },
+  {
+    "revision": "1a5f0bcb9890a6aad370b762de1b09bd",
+    "url": "/codabae/static/media/j.1a5f0bcb.JPG"
+  },
+  {
+    "revision": "176f3782623068d5f43694d8ab498c83",
+    "url": "/codabae/static/media/k.176f3782.JPG"
+  },
+  {
+    "revision": "bd4cfba14195c94b6134a6ee1f5bf754",
+    "url": "/codabae/static/media/l.bd4cfba1.JPG"
+  },
+  {
+    "revision": "6547599656a366d099d5c07d7d91b65f",
+    "url": "/codabae/static/media/m.65475996.JPG"
+  },
+  {
+    "revision": "b424fc53b8d78f93824c003cc48b9097",
+    "url": "/codabae/static/media/n.b424fc53.JPG"
+  },
+  {
+    "revision": "02560fa56b4413e4ab3bb7419f7fefc6",
+    "url": "/codabae/static/media/o.02560fa5.JPG"
+  },
+  {
+    "revision": "591e0ec4d5d5deebafd5c9314959fb8f",
+    "url": "/codabae/static/media/p.591e0ec4.JPG"
+  },
+  {
+    "revision": "543bb6fc1bb1605561f8104963eb082d",
+    "url": "/codabae/static/media/q.543bb6fc.jpg"
+  },
+  {
+    "revision": "a298f4641682b024785568055ef047cb",
+    "url": "/codabae/static/media/r.a298f464.jpg"
+  },
+  {
+    "revision": "434a946e4ae7026f0e987882b17c8d03",
+    "url": "/codabae/static/media/s.434a946e.jpg"
+  },
+  {
+    "revision": "7aecfc7508383b745b8145ed36cf168d",
+    "url": "/codabae/static/media/t.7aecfc75.jpg"
+  },
+  {
+    "revision": "bb1c351dc4e44a198f82f15debce038c",
+    "url": "/codabae/static/media/u.bb1c351d.jpg"
+  },
+  {
+    "revision": "9d98957001a8a6a9ea0ead9efce93dd5",
+    "url": "/codabae/static/media/v.9d989570.jpg"
+  },
+  {
+    "revision": "b88cd671d1aa38138a95cdff341d8153",
+    "url": "/codabae/static/media/w.b88cd671.jpg"
+  },
+  {
+    "revision": "9a83bf52dedebcc5b6b90b07f7fe682d",
+    "url": "/codabae/static/media/x.9a83bf52.jpg"
+  },
+  {
+    "revision": "9c4efb40caa4fe415b42030aa161ec1b",
+    "url": "/codabae/static/media/y.9c4efb40.jpg"
+  },
+  {
+    "revision": "4f4209c580747e156e59fc6ec49d85f5",
+    "url": "/codabae/static/media/z.4f4209c5.jpg"
+  },
+  {
+    "revision": "33be2d5744b48149dac57ae89b7bb860",
+    "url": "/codabae/index.html"
+  }
+];
